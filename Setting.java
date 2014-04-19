24dd22b1cb75d00de22b5ffcbb5bf1c3d5d1eb65
WeAreTogether <dishaponganti@gmail.com>
2014-04-20 00:47:15 +0530

    setting
    contains basic java class

---
+++ Setting.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Setting {
+	public static int weekHours = 60;
+    public static int dailyHours = 12;
+    public static int workingDays = 5;
+    static public final int selection_prefered = 0;
+    static public final int selection_available = 1;
+    static public final int selection_not_available = 2;
+    public static int populationSize = 50;
+    public static int noofrooms=13;
+}
